860a34263394f33825834ceec1fc6184abb7a50a
Broc Keeton <broc.keeton@student.centralia.edu>
2025-02-08 12:30:38 -0800

    Fixed login form and began work in customer cart

--- GourmetShopRepositoryTest/CustomerCart.Designer.cs
+++ GourmetShopRepositoryTest/CustomerCart.Designer.cs
@@ -31,31 +31,31 @@
             CartGridView = new DataGridView();
             Purchasebtn = new Button();
             Backtoformbtn = new Button();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
+            TotalItemslbl = new Label();
             label4 = new Label();
             label5 = new Label();
-            Subtotallbl = new Label();
-            Taxlbl = new Label();
             TotalAmountlbl = new Label();
             Removeitemcartbtn = new Button();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)CartGridView).BeginInit();
             SuspendLayout();
             // 
             // CartGridView
             // 
             CartGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            CartGridView.Location = new Point(458, 77);
+            CartGridView.Location = new Point(523, 103);
+            CartGridView.Margin = new Padding(3, 4, 3, 4);
             CartGridView.Name = "CartGridView";
-            CartGridView.Size = new Size(313, 343);
+            CartGridView.RowHeadersWidth = 51;
+            CartGridView.Size = new Size(358, 457);
             CartGridView.TabIndex = 0;
             // 
             // Purchasebtn
             // 
-            Purchasebtn.Location = new Point(275, 358);
+            Purchasebtn.Location = new Point(314, 477);
+            Purchasebtn.Margin = new Padding(3, 4, 3, 4);
             Purchasebtn.Name = "Purchasebtn";
-            Purchasebtn.Size = new Size(82, 47);
+            Purchasebtn.Size = new Size(94, 63);
             Purchasebtn.TabIndex = 1;
             Purchasebtn.Text = "Purchase Cart";
             Purchasebtn.UseVisualStyleBackColor = true;
@@ -63,113 +63,86 @@
             // 
             // Backtoformbtn
             // 
-            Backtoformbtn.Location = new Point(59, 358);
+            Backtoformbtn.Location = new Point(67, 477);
+            Backtoformbtn.Margin = new Padding(3, 4, 3, 4);
             Backtoformbtn.Name = "Backtoformbtn";
-            Backtoformbtn.Size = new Size(82, 47);
+            Backtoformbtn.Size = new Size(94, 63);
             Backtoformbtn.TabIndex = 2;
             Backtoformbtn.Text = "Back to Products";
             Backtoformbtn.UseVisualStyleBackColor = true;
             Backtoformbtn.Click += Backtoformbtn_Click;
             // 
-            // label1
+            // TotalItemslbl
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(59, 82);
-            label1.Name = "label1";
-            label1.Size = new Size(54, 15);
-            label1.TabIndex = 3;
-            label1.Text = "Subtotal:";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(554, 41);
-            label2.Name = "label2";
-            label2.Size = new Size(116, 15);
-            label2.TabIndex = 4;
-            label2.Text = "Num of items in cart";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(59, 131);
-            label3.Name = "label3";
-            label3.Size = new Size(27, 15);
-            label3.TabIndex = 5;
-            label3.Text = "Tax:";
+            TotalItemslbl.AutoSize = true;
+            TotalItemslbl.Location = new Point(712, 66);
+            TotalItemslbl.Name = "TotalItemslbl";
+            TotalItemslbl.Size = new Size(70, 20);
+            TotalItemslbl.TabIndex = 4;
+            TotalItemslbl.Text = "$Amount";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(59, 180);
+            label4.Location = new Point(67, 240);
             label4.Name = "label4";
-            label4.Size = new Size(307, 15);
+            label4.Size = new Size(369, 20);
             label4.TabIndex = 6;
             label4.Text = "------------------------------------------------------------";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(59, 232);
+            label5.Location = new Point(67, 309);
             label5.Name = "label5";
-            label5.Size = new Size(106, 15);
+            label5.Size = new Size(133, 20);
             label5.TabIndex = 7;
             label5.Text = "Total Amount Due:";
-            // 
-            // Subtotallbl
-            // 
-            Subtotallbl.AutoSize = true;
-            Subtotallbl.Location = new Point(227, 83);
-            Subtotallbl.Name = "Subtotallbl";
-            Subtotallbl.Size = new Size(57, 15);
-            Subtotallbl.TabIndex = 8;
-            Subtotallbl.Text = "$Amount";
-            // 
-            // Taxlbl
-            // 
-            Taxlbl.AutoSize = true;
-            Taxlbl.Location = new Point(231, 129);
-            Taxlbl.Name = "Taxlbl";
-            Taxlbl.Size = new Size(57, 15);
-            Taxlbl.TabIndex = 9;
-            Taxlbl.Text = "$Amount";
             // 
             // TotalAmountlbl
             // 
             TotalAmountlbl.AutoSize = true;
-            TotalAmountlbl.Location = new Point(222, 229);
+            TotalAmountlbl.Location = new Point(257, 309);
             TotalAmountlbl.Name = "TotalAmountlbl";
-            TotalAmountlbl.Size = new Size(57, 15);
+            TotalAmountlbl.Size = new Size(70, 20);
             TotalAmountlbl.TabIndex = 10;
             TotalAmountlbl.Text = "$Amount";
             // 
             // Removeitemcartbtn
             // 
-            Removeitemcartbtn.Location = new Point(164, 358);
+            Removeitemcartbtn.Location = new Point(187, 477);
+            Removeitemcartbtn.Margin = new Padding(3, 4, 3, 4);
             Removeitemcartbtn.Name = "Removeitemcartbtn";
-            Removeitemcartbtn.Size = new Size(82, 47);
+            Removeitemcartbtn.Size = new Size(94, 63);
             Removeitemcartbtn.TabIndex = 11;
             Removeitemcartbtn.Text = "Remove item";
             Removeitemcartbtn.UseVisualStyleBackColor = true;
             Removeitemcartbtn.Click += Removeitemcartbtn_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(523, 66);
+            label1.Name = "label1";
+            label1.Size = new Size(147, 20);
+            label1.TabIndex = 12;
+            label1.Text = "Num of items in cart:";
+            // 
             // CustomerCart
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Controls.Add(label1);
             Controls.Add(Removeitemcartbtn);
             Controls.Add(TotalAmountlbl);
-            Controls.Add(Taxlbl);
-            Controls.Add(Subtotallbl);
             Controls.Add(label5);
             Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            Controls.Add(TotalItemslbl);
             Controls.Add(Backtoformbtn);
             Controls.Add(Purchasebtn);
             Controls.Add(CartGridView);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "CustomerCart";
             Text = "CustomerCart";
             ((System.ComponentModel.ISupportInitialize)CartGridView).EndInit();
@@ -182,14 +155,11 @@
         private DataGridView CartGridView;
         private Button Purchasebtn;
         private Button Backtoformbtn;
-        private Label label1;
-        private Label label2;
-        private Label label3;
+        private Label TotalItemslbl;
         private Label label4;
         private Label label5;
-        private Label Subtotallbl;
-        private Label Taxlbl;
         private Label TotalAmountlbl;
         private Button Removeitemcartbtn;
+        private Label label1;
     }
 }
--- GourmetShopRepositoryTest/CustomerCart.cs
+++ GourmetShopRepositoryTest/CustomerCart.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using GourmetShopLibrary.Models;
+using GourmetShopLibrary.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -12,30 +15,94 @@ namespace GourmetShopRepositoryTest
 {
     public partial class CustomerCart : Form
     {
-        public CustomerCart()
+        private List<Product> cartlist = new List<Product>();
+        string conn = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+        public CustomerCart(List<Product> products)
         {
             InitializeComponent();
-            // calculate lables with monitary amounts
-            // update label with number of items in list
+            cartlist = products;
+            LoadCart();
+            UpdateCartSummary();
+        }
+
+        private void LoadCart()
+        {
+            CartGridView.Rows.Clear();
+            foreach (var product in cartlist)
+            {
+                //add products to cart 
+                CartGridView.Rows.Add();
+            }
+            UpdateCartSummary();
+        }
+
+        private void UpdateCartSummary()
+        {
+            decimal totalAmount = 0;
+            int totalItems = 0;
+
+            foreach (var product in cartlist)
+            {
+                totalAmount += product.UnitPrice;//need to get quantity somehow
+                //totalItems+= product.Quantity; somehow need to get quantity
+            }
+
+            TotalAmountlbl.Text = totalAmount.ToString();
+            TotalItemslbl.Text = totalItems.ToString();
         }
 
         private void Backtoformbtn_Click(object sender, EventArgs e)
         {
-            // link back to first form
+            CustomerSupplierLookup customerSupplierLookup = new CustomerSupplierLookup();
+            customerSupplierLookup.ShowDialog();
+            this.Close();
         }
 
         private void Removeitemcartbtn_Click(object sender, EventArgs e)
         {
             // Remove an item form list
-            // update lable amounts
-            // Update lable with amount of items in cart
+            if (cartlist.Count > 0) 
+            { 
+                cartlist.RemoveAt(cartlist.Count - 1); 
+                CartGridView.Rows.RemoveAt(CartGridView.Rows.Count - 1);
+                UpdateCartSummary();
+            }
+            else
+            {
+                MessageBox.Show("Cart is empty", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
 
         private void Purchasebtn_Click(object sender, EventArgs e)
         {
-            // call SP for insert on order
-            // call SP for 
-            // grab the date for SP orders and pass
+            if (cartlist.Count == 0)
+            {
+                MessageBox.Show("Cart is empty", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                //OrderRepository orderRepository = new OrderRepository(conn);
+                //int customerId = 1; // need to get customer id from login
+                //int orderId = orderRepository.AddOrder(customerId, DateTime.Now);
+
+                //OrderItemRepository orderItemRepository = new OrderItemRepository(conn);
+                //foreach (var product in cartlist)
+                //{
+                //    orderItemRepository.AddOrderItem(orderId, product.ProductID, product.UnitPrice, 1);//need to get quantity once repository is finished
+                //}
+
+                MessageBox.Show("Order placed successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                cartlist.Clear();
+                CartGridView.Rows.Clear();
+                UpdateCartSummary();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error placing order: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
